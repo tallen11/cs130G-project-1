3cfbbb7ce8216e9919e9c99754d15e253d391bc3
Tate Allen <tatea7@yahoo.com>
2016-03-31 12:34:13 -0700

    Chords and some modularity added

--- sound.c
+++ sound.c
@@ -10,13 +10,18 @@
 const int WAV_SAMPLES_PER_SEC = 44100;
 const int MAX_VOLUME = 32000;
 
-void writeTonewriteToneSineWave(FILE *file, Tone tone);
-void writeTonewriteToneSquareWave(FILE *file, Tone tone);
-void writeChord();
+void writeToneData(FILE *file, double *toneData, uint32_t length);
+void writeToneSineWave(FILE *file, Tone tone);
+void writeToneSquareWave(FILE *file, Tone tone);
+void writeChord(FILE *file, uint32_t length);
 void writeLoop(FILE *file, uint32_t loopCount, ToneSequence *sequence);
 double envelope(uint32_t x, uint32_t len);
 void normalizeData(double *data, uint32_t length);
 int16_t* packageData(double *data, uint32_t length);
+
+void addSineWave(double *data, uint32_t length, Tone tone);
+void addSquareWave(double *data, uint32_t length, Tone tone);
+void buildChord(double *data, uint32_t length, Tone tone1, Tone tone2, Tone tone3);
 
 double fRand(double fMin, double fMax)
 {
@@ -28,75 +33,138 @@ int main(/*int argc, char const *argv[]*/)
 {
 	FILE *file = wavfile_open("sound.wav");
 
+	writeChord(file, 5);
+
 	// srand(time(NULL));
 
-	ToneSequence *seq = make_sequence();
-	for (int i = 0; i < 15; ++i) {
-		// uint32_t randIndex = rand() % 7;
-		add_tone(seq, make_tone(c_scale[i > 7 ? 14 - i : i] * 7, 0.5, 0));
-	}
+	// ToneSequence *seq = make_sequence();
+	// for (int i = 0; i < 15; ++i) {
+	// 	// uint32_t randIndex = rand() % 7;
+	// 	add_tone(seq, make_tone(c_scale[i > 7 ? 14 - i : i] * 7, 0.1, 6));
+	// }
 
-	// add_tone(seq, make_tone(659.26, 0.5));
-	// add_tone(seq, make_tone(587.33, 0.5));
-	// add_tone(seq, make_tone(523.25, 0.5));
-	// add_tone(seq, make_tone(0.0, 0.5));
+	// // add_tone(seq, make_tone(659.26, 0.5));
+	// // add_tone(seq, make_tone(587.33, 0.5));
+	// // add_tone(seq, make_tone(523.25, 0.5));
+	// // add_tone(seq, make_tone(0.0, 0.5));
 
-	writeLoop(file, 5, seq);
+	// writeLoop(file, 5, seq);
 	wavfile_close(file);
 
 	return 0;
 }
 
+void writeToneData(FILE *file, double *toneData, uint32_t length)
+{
+	normalizeData(toneData, length);
+	int16_t *waveformData = packageData(toneData, length);
+	wavfile_write(file, waveformData, length);
+	free(waveformData);
+}
+
 void writeToneSineWave(FILE *file, Tone tone)
 {
 	uint32_t sampleCount = ceil(WAV_SAMPLES_PER_SEC * tone.length);
-	double data[sampleCount];
-	for (uint32_t i = 0; i < sampleCount; ++i) {
-		double step = (double)i / WAV_SAMPLES_PER_SEC;
-		data[i] = sin(tone.frequency * step * 2 * M_PI);
-		for (uint32_t h = 2; h < tone.harmonics + 2; ++h) {
-			data[i] += sin(tone.frequency * h * step * 2 * M_PI);
-		}
-	}
+	double *data = malloc(sizeof(double) * sampleCount);
+	addSineWave(data, sampleCount, tone);
+	writeToneData(file, data, sampleCount);
+	free(data);
 
-	normalizeData(data, sampleCount);
-	int16_t *waveformData = packageData(data, sampleCount);
-
-	wavfile_write(file, waveformData, sampleCount);
+	// for (uint32_t i = 0; i < sampleCount; ++i) {
+	// 	double step = (double)i / WAV_SAMPLES_PER_SEC;
+	// 	data[i] = sin(tone.frequency * step * 2 * M_PI);
+	// 	for (uint32_t h = 2; h < tone.harmonics + 2; ++h) {
+	// 		data[i] += sin(tone.frequency * h * step * 2 * M_PI);
+	// 	}
+	// }
 }
 
 void writeToneSquareWave(FILE *file, Tone tone)
 {
 	uint32_t sampleCount = ceil(WAV_SAMPLES_PER_SEC * tone.length);
-	int16_t waveformData[sampleCount];
-	for (uint32_t i = 0; i < sampleCount; ++i) {
-		double step = (double)i / WAV_SAMPLES_PER_SEC;
-		waveformData[i] = envelope(i, sampleCount) * MAX_VOLUME * (sin(tone.frequency * step * 2 * M_PI) > 0.0 ? 1 : -1);
-	}
+	double *data = malloc(sizeof(double) * sampleCount);
+	addSquareWave(data, sampleCount, tone);
+	writeToneData(file, data, sampleCount);
+	free(data);
 
-	wavfile_write(file, waveformData, sampleCount);
+	// int16_t waveformData[sampleCount];
+	// for (uint32_t i = 0; i < sampleCount; ++i) {
+	// 	double step = (double)i / WAV_SAMPLES_PER_SEC;
+	// 	waveformData[i] = envelope(i, sampleCount) * MAX_VOLUME * (sin(tone.frequency * step * 2 * M_PI) > 0.0 ? 1 : -1);
+	// }
+
+	// wavfile_write(file, waveformData, sampleCount);
 }
 
 void writeLoop(FILE *file, uint32_t loopCount, ToneSequence *toneSequence)
 {
 	for (uint32_t loop = 0; loop < loopCount; ++loop) {
 		for (uint32_t i = 0; i < toneSequence->toneCount; ++i) {
-			// writeToneSineWave(file, toneSequence->tones[i]);
-			writeToneSquareWave(file, toneSequence->tones[i]);
+			writeToneSineWave(file, toneSequence->tones[i]);
+			// writeToneSquareWave(file, toneSequence->tones[i]);
 		}
 	}
 }
 
-void writeChord()
+void writeChord(FILE *file, uint32_t length)
 {
-	/*
-b(t) = sin(Api(t)) is the base note of the chord at frequency A.
-T(t) = sin(5/4piA(t)) is the major third of the base b(t).
-D(t) = sin(3/2piA(t)) is the dominant (fifth) of the base b(t).
-A(t) = sin(2Api(t)) is the octave.
-	*/
+	uint32_t sampleCount = ceil(WAV_SAMPLES_PER_SEC * length);
+	double *data = malloc(sizeof(double) * sampleCount);
+	buildChord(data, sampleCount, 
+					 make_tone(C1 * 7, length, 0),
+					 make_tone(E1 * 7, length, 0),
+					 make_tone(G1 * 7, length, 0));
 
+	writeToneData(file, data, sampleCount);
+	free(data);
 
+	// uint32_t sampleCount = ceil(WAV_SAMPLES_PER_SEC * length);
+	// double *data = malloc(sizeof(double) * sampleCount);
+	// for (uint32_t i = 0; i < sampleCount; ++i) {
+	// 	double step = (double)i / WAV_SAMPLES_PER_SEC;
+	// 	data[i] = sin(C1 * 7 * step * 2 * M_PI);
+	// 	data[i] += sin(E1 * 7 * step * 2 * M_PI);
+	// 	data[i] += sin(G1 * 7 * step * 2 * M_PI);
+	// 	// for (uint32_t h = 2; h < tone.harmonics + 2; ++h) {
+	// 	// 	data[i] += sin(tone.frequency * h * step * 2 * M_PI);
+	// 	// }
+	// }
+
+	// writeToneData(file, data, sampleCount);
+	// free(data);
+}
+
+void addSineWave(double *data, uint32_t length, Tone tone)
+{
+	for (uint32_t i = 0; i < length; ++i) {
+		double step = (double)i / WAV_SAMPLES_PER_SEC;
+		data[i] += sin(tone.frequency * step * 2 * M_PI);
+		for (uint32_t h = 2; h < tone.harmonics + 2; ++h) {
+			data[i] += sin(tone.frequency * h * step * 2 * M_PI);
+		}
+	}
+}
+
+void addSquareWave(double *data, uint32_t length, Tone tone)
+{
+	for (uint32_t i = 0; i < length; ++i) {
+		double step = (double)i / WAV_SAMPLES_PER_SEC;
+		data[i] += (sin(tone.frequency * step * 2 * M_PI) > 0.0 ? 1.0 : -1.0);
+		for (uint32_t h = 2; h < tone.harmonics + 2; ++h) {
+			data[i] += (sin(tone.frequency * h * step * 2 * M_PI) > 0.0 ? 1.0 : -1.0);
+		}
+	}
+}
+
+void buildChord(double *data, uint32_t length, Tone tone1, Tone tone2, Tone tone3)
+{
+	// addSineWave(data, length, tone1);
+	// addSineWave(data, length, tone2);
+	// addSineWave(data, length, tone3);
+
+	addSquareWave(data, length, tone1);
+	addSquareWave(data, length, tone2);
+	addSquareWave(data, length, tone3);
 }
 
 double envelope(uint32_t x, uint32_t len)
@@ -129,9 +197,9 @@ void normalizeData(double *data, uint32_t length)
 
 int16_t* packageData(double *data, uint32_t length)
 {
-	int16_t waveformData[length];
+	int16_t *waveformData = malloc(sizeof(int16_t) * length);
 	for (uint32_t i = 0; i < length; ++i) {
-		waveformData[i] = envelope(i, length) * MAX_VOLUME * data[i];
+		waveformData[i] = data[i] * envelope(i, length) * MAX_VOLUME;
 	}
 
 	return waveformData;
